4878e1b1dbae2cf601ed860162c3a1a242a7a7b1
Yunseon Im <yim@c4r6s5.42seoul.kr>
2023-02-13 21:05:20 +0900

    exit

--- srcs/builtins/ft_exit.c
+++ srcs/builtins/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: yim <yim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/13 15:50:14 by yim               #+#    #+#             */
-/*   Updated: 2023/02/13 20:55:07 by yim              ###   ########.fr       */
+/*   Updated: 2023/02/13 21:05:05 by yim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	check_digit(t_command *cmd)
 	int	i;
 
 	i = 0;
+	if ((cmd->cmd)[1] == NULL)
+		return (0);
 	while ((cmd->cmd)[1][i])
 	{
 		if (!ft_isdigit((cmd->cmd[1][i])))
@@ -35,7 +37,7 @@ int	ft_exit(t_command *cmd)
 		ft_putstr_fd("exit\n", cmd->file_out_fd);
 	if (check_digit(cmd))
 		ft_putstr_fd("exit: numeric argument required\n", 2);
-	else if ((cmd->cmd)[2] != NULL)
+	else if ((cmd->cmd)[1] != NULL && (cmd->cmd)[2] != NULL)
 		ft_putstr_fd("exit: too many arguments\n", 2);
 	else
 		exit(0);
