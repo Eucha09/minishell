f7c9823cf0af43cdce78485a6d44ae67a7498e30
Yunseon Im <yim@c4r6s5.42seoul.kr>
2023-02-15 14:36:28 +0900

    ing

--- includes/builtins.h
+++ includes/builtins.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtins.h                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: eujeong <eujeong@student.42.fr>            +#+  +:+       +#+        */
+/*   By: yim <yim@student.42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/25 15:25:26 by yim               #+#    #+#             */
-/*   Updated: 2023/02/15 14:33:53 by eujeong          ###   ########.fr       */
+/*   Updated: 2023/02/15 14:35:17 by yim              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,6 @@
 # define FALSE 0
 
 //error
-int		code_error(char *str);
 int		free_code_error(char *free_str, int i);
 //cd
 int		cd(char **envp, char *str);
